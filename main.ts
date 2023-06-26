8a0adc1742fd521950f593849429f73f59819ebb
ihch <nemu0sou@gmail.com>
2023-06-26 23:04:42 +0900

    テキストのHTML化を追加

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
+type Node = {
+  type: 'TEXT';
+  text: string;
+  node: Node[] | null;
+}
+
+function parse(markdown: string): Node {
+  return { type: 'TEXT', text: markdown, node: null }
+}
+
+function renderHTML(node: Node): string {
+  if (node.type === 'TEXT') {
+    return `<p>${node.text}</p>`;
+  }
+  return '';
+}
+
 export function convertMarkdownToHTML(markdown: string): string {
-  return markdown;
+  const ast = parse(markdown);
+  return renderHTML(ast);
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  convertMarkdownToHTML("Markdown");
+  const result = convertMarkdownToHTML("Markdown");
+  console.log(result);
 }
--- main_test.ts
+++ main_test.ts
@@ -2,5 +2,5 @@ import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 import { convertMarkdownToHTML } from "./main.ts";
 
 Deno.test(function parseTest() {
-  assertEquals(convertMarkdownToHTML("Markdown"), "Markdown");
+  assertEquals(convertMarkdownToHTML("Markdown"), "<p>Markdown</p>");
 });
