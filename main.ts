d6999541002986d14cc5a4831546afaade1856db
ihch <nemu0sou@gmail.com>
2023-06-27 00:08:02 +0900

    HeadingのHTML化を追加

--- main.ts
+++ main.ts
@@ -1,27 +1,65 @@
+const heading1_regexp = /^(?<level>#+)\s+(?<content>\S+)$/;
+
 type Node = {
-  type: 'TEXT';
+  type: "TEXT" | "HEADING";
   text: string;
+  tag: string;
   node: Node[] | null;
-}
+};
 
-function parse(markdown: string): Node {
-  return { type: 'TEXT', text: markdown, node: null }
-}
+function parse(markdown: string): Node[] {
+  const markdownRoots = markdown.split("\n");
+  const ast: Node[] = [];
 
-function renderHTML(node: Node): string {
-  if (node.type === 'TEXT') {
-    return `<p>${node.text}</p>`;
+  for (let i = 0; i < markdownRoots.length; i++) {
+    const line = markdownRoots[i].trim();
+
+    if (line === "") continue;
+
+    const heading1 = line.match(heading1_regexp);
+    if (heading1) {
+      const level = heading1.groups?.level.length || 1;
+      const content = heading1.groups?.content || "";
+      ast.push({
+        type: "HEADING",
+        text: content,
+        tag: `h${level}`,
+        node: null,
+      });
+    } else {
+      ast.push({ type: "TEXT", text: line, tag: `p`, node: null });
+    }
   }
-  return '';
+
+  return ast;
+}
+
+function renderHTML(ast: Node[]): string {
+  const html = ast.reduce(
+    (prev, cur) => {
+      return prev + `<${cur.tag}>${cur.text}</${cur.tag}>`;
+    },
+    "",
+  );
+  return html;
 }
 
 export function convertMarkdownToHTML(markdown: string): string {
-  const ast = parse(markdown);
+  const ast = parse(markdown.trim());
   return renderHTML(ast);
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const result = convertMarkdownToHTML("Markdown");
+  const markdown = `
+    # Heading1
+
+    text
+
+    # Heading2
+
+    text
+    `;
+  const result = convertMarkdownToHTML(markdown);
   console.log(result);
 }
--- main_test.ts
+++ main_test.ts
@@ -4,3 +4,24 @@ import { convertMarkdownToHTML } from "./main.ts";
 Deno.test(function parseTest() {
   assertEquals(convertMarkdownToHTML("Markdown"), "<p>Markdown</p>");
 });
+
+Deno.test(function headingTest() {
+  assertEquals(convertMarkdownToHTML("# Markdown"), "<h1>Markdown</h1>");
+  assertEquals(convertMarkdownToHTML("## Markdown"), "<h2>Markdown</h2>");
+  assertEquals(convertMarkdownToHTML("### Markdown"), "<h3>Markdown</h3>");
+  assertEquals(convertMarkdownToHTML("#### Markdown"), "<h4>Markdown</h4>");
+});
+
+Deno.test(function multilineTest() {
+  assertEquals(convertMarkdownToHTML(
+    `
+    # Heading1
+
+    text
+
+    ## Heading2
+
+    text
+    `
+  ), "<h1>Heading1</h1><p>text</p><h2>Heading2</h2><p>text</p>");
+});
