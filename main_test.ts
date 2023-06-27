92ca340dff0e1fa480fd9a962aee634f76cb882f
ihch <nemu0sou@gmail.com>
2023-06-27 13:28:35 +0900

    テスト追加

--- main_test.ts
+++ main_test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 import { convertMarkdownToHTML } from "./main.ts";
 
 Deno.test(function parseTest() {
-  assertEquals(convertMarkdownToHTML("Markdown"), "<p>Markdown</p>");
+  assertEquals(convertMarkdownToHTML("Markdown"), "Markdown");
 });
 
 Deno.test(function headingTest() {
@@ -12,16 +12,40 @@ Deno.test(function headingTest() {
   assertEquals(convertMarkdownToHTML("#### Markdown"), "<h4>Markdown</h4>");
 });
 
-Deno.test(function multilineTest() {
-  assertEquals(convertMarkdownToHTML(
-    `
-    # Heading1
+Deno.test(function strongTest() {
+  assertEquals(
+    convertMarkdownToHTML("**Strong Hello**"),
+    "<strong>Strong Hello</strong>",
+  );
+});
 
-    text
+Deno.test(function italicTest() {
+  assertEquals(convertMarkdownToHTML("*Italic Hello*"), "<i>Italic Hello</i>");
+});
+
+Deno.test(function codeTest() {
+  assertEquals(
+    convertMarkdownToHTML("`console.log('Hello, world!')`"),
+    "<pre>console.log('Hello, world!')</pre>",
+  );
+});
+
+Deno.test(function multilineTest() {
+  assertEquals(
+    convertMarkdownToHTML(
+    `
+    # Heading1 **bold**
+
+    text **strong Hello**
 
     ## Heading2
 
-    text
-    `
-  ), "<h1>Heading1</h1><p>text</p><h2>Heading2</h2><p>text</p>");
+    text *italic Hello*
+
+    \`console.log("Hello, world!")\``
+    ),
+    `<h1>Heading1 <strong>bold</strong></h1>
+    text <strong>strong Hello</strong><h2>Heading2</h2>    
+    text <i>italic Hello</i><pre>console.log("Hello, world!")</pre>    `,
+  );
 });
--- regexp.ts
+++ regexp.ts
@@ -1,6 +1,6 @@
 export const heading_regexp = /^(?<level>#+)\s+(?<content>.+)/;
 
 // inline
-export const strong_regexp = /\s\*\*(?<content>.*)\*\*\s?/;
-export const italic_regexp = /\s\*(?<content>.*)\*\s?/;
-export const code_regexp = /\s`(?<content>.*)`\s?/;
+export const strong_regexp = /\*\*(?<content>.*)\*\*\s?/;
+export const italic_regexp = /\*(?<content>.*)\*\s?/;
+export const code_regexp = /`(?<content>.*)`\s?/;
